674c8367bf46b6c1fb10b0290e447c9618d8dfb9
Jezebel1990 <jezebelguedes@gmail.com>
2023-06-06 22:20:05 -0300

    create game

---
+++ src/components/game.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="game">
+
+    </div >
+</template>
+
+<script>
+export default{
+    name: 'Jogo',
+    props:{
+  
+    },
+    data(){
+        return {
+          
+        }
+    },
+    methods: {
+      
+    }
+}
+</script>
+
+<style>
+ 
+</style>
